81d6582a5ea3c7f4ed06d65154a2711e22b26ccd
webmegood <webmegood@gmail.com>
2018-01-03 00:15:42 +1100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,15 @@
 $(function() {
-    $('body').on('mousedown', 'div', function() {
-        $(this).addClass('draggable').parents().on('mousemove', function(e) {
+    $('body').on('vmousedown', 'div', function() {
+        $(this).addClass('draggable').parents().on('vmousemove', function(e) {
             $('.draggable').offset({
                 top: e.pageY - $('.draggable').outerHeight() / 2,
                 left: e.pageX - $('.draggable').outerWidth() / 2
-            }).on('mouseup', function() {
+            }).on('vmouseup', function() {
                 $(this).removeClass('draggable');
             });
         });
         e.preventDefault();
-    }).on('mouseup', function() {
+    }).on('vmouseup', function() {
         $('.draggable').removeClass('draggable');
     });
 });
