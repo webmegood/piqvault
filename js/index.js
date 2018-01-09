3454eaf7c0ba8543abcb2db7d0aa5aaf1005edbb
webmegood <webmegood@gmail.com>
2018-01-09 12:03:33 +1100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,44 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
+    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        // ...
-
-        /* 1) Add a click handler for your button */
-        document.getElementById('launch-editor').addEventListener('click', this.launchEditor, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
+    // Update DOM on a Received Event
     receivedEvent: function(id) {
-        // ...
-    },
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-    /* 2) Make a helper function to launch the Image Editor */
-    launchEditor: function() {
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-    	/* 2.a) Prep work for calling `.edit()` */
-        function success(newUrl) {
-            console.log("Success!", newUrl);
-        }
-
-        function error(error) {
-            console.log("Error!", error);
-        }
-
-        var imageUrl = "../images/ernie.jpg";
-
-        var options = {
-            outputType: CSDKImageEditor.OutputType.JPEG,
-            tools: [
-                CSDKImageEditor.ToolType.EFFECTS,
-                CSDKImageEditor.ToolType.CROP
-            ],
-            quality: 50
-        };
-
-        /* 2.b) Launch the Image Editor */
-        CSDKImageEditor.edit(success, error, imageUrl, options);
+        console.log('Received Event: ' + id);
     }
 };
